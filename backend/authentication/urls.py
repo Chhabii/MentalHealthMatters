6b2b6f2f64303e052341e390b2f6d89d132bd4ea
bharat <thapabharat123456@gmail.com>
2022-12-19 20:41:44 +0545

    Added Profile Update
    when user goes to /account/profile their data are loaded in the form. They can edit those data and submit form.
    profileupdate in view will handle the request and update the details and redirect to /account/profile showing flash message.
    address field along with other fields depends on other models which are not complete hence they are not updated.

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('dashboard/',views.dashboard,name='dashboard'),
     path('logout/',views.log_out,name='logout'),
     path('changepass/',views.user_change_pass,name='changepass'),
+    path('profile/',views.profile,name='profile'),
+    path('profileupdate/',views.profileupdate,name='profileupdate'),
+
     # path('forgot-password/', auth_views.PasswordResetView.as_view(), name='forgot_password'),
 ]
