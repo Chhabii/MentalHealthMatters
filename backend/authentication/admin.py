377e7c8b358e17d7cd236f4b028cae8ebbfdddf0
bharat <thapabharat123456@gmail.com>
2022-12-21 16:10:03 +0545

    some changes

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -7,22 +7,22 @@ from django.contrib.auth.admin import UserAdmin
 class UserModel(UserAdmin):
     list_display = ['id','username','user_type']
 
-@admin.register(Admin)
-class userAdmin(admin.ModelAdmin):
-    list_display = ['id']
+# @admin.register(Admin)
+# class userAdmin(admin.ModelAdmin):
+#     list_display = ['id']
 
-@admin.register(Teacher)
-class TeacherAdmin(admin.ModelAdmin):
-    list_display = ['id']
+# @admin.register(Teacher)
+# class TeacherAdmin(admin.ModelAdmin):
+#     list_display = ['id']
 
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ['id']
+# @admin.register(Student)
+# class StudentAdmin(admin.ModelAdmin):
+#     list_display = ['id']
 
 
 
 admin.site.register(CustomUser,UserModel)
-# admin.site.register(Teacher)
-# admin.site.register(Student)
-# admin.site.register(Admin)
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Admin)
 
